6162fe12ade6480f23fbb45abc781d1d2bc88481
rodrigopulse <contato@rodrigogodoy.net>
2019-08-07 11:58:24 -0300

    Primeiro Commit

---
+++ Gulpfile.js
@@ -0,0 +1,67 @@
+"use strict";
+/**
+ * Imports
+ */
+const gulp          = require('gulp');
+const sass          = require('gulp-sass');
+const cssnano       = require('gulp-cssnano');
+const sourcemaps    = require('gulp-sourcemaps');
+const concat        = require('gulp-concat');
+const uglify        = require('gulp-uglify');
+const phplint       = require('gulp-phplint');
+const rename		= require('gulp-rename');
+
+/**
+ * Sass
+ */
+function css() {
+    return  gulp
+    .src('assets/sass/load.scss')
+    .pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: "expanded"}))
+		.pipe(cssnano())
+		.pipe(rename('style.css'))
+    .pipe(sourcemaps.write('./assets/sourcemaps'))
+    .pipe(gulp.dest('./'));
+}
+
+/**
+ * JavaScript
+ */
+var jsFiles = [
+	'./assets/js/scripts.js'
+];
+function js() {
+    return (
+        gulp
+        .src(jsFiles)
+        .pipe(concat('./scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'))
+    );
+}
+
+/**
+ * PHP Task
+ */
+function php() {
+    return (
+        gulp
+        .src('./**/*.php')
+        .pipe(phplint('', {skipPassedFiles: true}))
+    );
+}
+/**
+ * Watch Task
+ */
+function watchFiles() {
+    gulp.watch('assets/sass/**/*.scss', gulp.series(css));
+    gulp.watch(jsFiles, gulp.series(js));
+    gulp.watch('./**/*.php', gulp.series(php));
+}
+
+const build = gulp.series(css, js, php);
+const watch = gulp.series(watchFiles);
+
+exports.default = build;
+exports.watch = watch;
---
+++ README.md
@@ -0,0 +1,18 @@
+# Pagu Pictures
+Template feito para a Pagu Pictures
+
+## Instalação
+
+### 1. Configurando
+Instale as dependências do npm
+
+`npm install`
+
+### 2. Desenvolvendo
+No gulpfile está tudo o que você precisa para desenvolver o template. Para rodar o gulp, basta rodar no terminal
+
+`gulp`
+
+Use o watch com
+
+`gulp watch`
