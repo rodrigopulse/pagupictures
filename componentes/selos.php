0de97555c1611ad21f12a198fa524ee8f06eaff2
rodrigopulse <contato@rodrigogodoy.net>
2019-08-21 14:30:06 -0300

    criado os selos em breve e catalogo

--- componentes/selos.php
+++ componentes/selos.php
@@ -3,6 +3,12 @@
     if( $selo === 'cartaz' ){
         $corSelo    = 'cartaz';
         $textoSelo  = 'Em Cartaz';
+    } else if($selo === 'embreve') {
+        $corSelo    = 'embreve';
+        $textoSelo  = 'Em Breve';
+    } else if($selo === 'catalogo') {
+        $corSelo    = 'catalogo';
+        $textoSelo  = 'Catálogo';
     } ?>
 
     <span class="selo selo--<?php echo $corSelo; ?>"><?php echo $textoSelo; ?></span>
