591973f4097293564657d6363f198dba0d40d703
rodrigopulse <contato@rodrigogodoy.net>
2019-08-30 16:40:47 -0300

    trocado o estilo de filtros

--- componentes/filtros.php
+++ componentes/filtros.php
@@ -6,24 +6,28 @@
             $classeEmbreve  = ' botao-padrao--desabilitado';
             $classeCatalogo = ' botao-padrao--desabilitado';
             $classeEmCasa   = ' botao-padrao--desabilitado';
-        } elseif ($filtro === 'em-breve') {
+            $classeTodos    = ' botao-padrao--desabilitado';
+        } elseif ($filtro === 'embreve') {
             $nome_filtro    = 'Em Breve';
             $classeCartaz   = ' botao-padrao--desabilitado';
             $classeEmbreve  = ' botao-padrao--ativo';
             $classeCatalogo = ' botao-padrao--desabilitado';
             $classeEmCasa   = ' botao-padrao--desabilitado';
+            $classeTodos    = ' botao-padrao--desabilitado';
         } elseif ($filtro === 'catalogo') {
             $nome_filtro    = 'Catálogo';
             $classeCartaz   = ' botao-padrao--desabilitado';
             $classeEmbreve  = ' botao-padrao--desabilitado';
             $classeCatalogo = ' botao-padrao--ativo';
             $classeEmCasa   = ' botao-padrao--desabilitado';
+            $classeTodos    = ' botao-padrao--desabilitado';
         } elseif ($filtro === 'emcasa') {
             $nome_filtro    = 'Assistir em Casa';
             $classeCartaz   = ' botao-padrao--desabilitado';
             $classeEmbreve  = ' botao-padrao--desabilitado';
             $classeCatalogo = ' botao-padrao--desabilitado';
             $classeEmCasa   = ' botao-padrao--ativo';
+            $classeTodos    = ' botao-padrao--desabilitado';
         }
     } else {
         $nome_filtro    = 'Filtros';
@@ -31,6 +35,7 @@
         $classeEmbreve  = ' botao-padrao--desabilitado';
         $classeCatalogo = ' botao-padrao--desabilitado';
         $classeEmCasa   = ' botao-padrao--desabilitado';
+        $classeTodos    = ' botao-padrao--ativo';
     } ?>
 
     <?php if(wp_is_mobile()) { ?>
@@ -40,13 +45,16 @@
             </div>
             <div class="accordion-filho">
                 <div class="row">
+                    <a class="botao-padrao <?php echo "$classeTodos"; ?>" href="/filmes">Todos</a>
+                </div>
+                <div class="row">
                     <div class="accordion-filho__lugar col-sm-12">
                         <a class="botao-padrao <?php echo "$classeCartaz"; ?>" href="/filmes?filtro=cartaz">Em Cartaz</a>
                     </div>
                 </div>
                 <div class="row">
                     <div class="accordion-filho__lugar col-sm-12">
-                        <a class="botao-padrao <?php echo "$classeEmbreve"; ?>" href="/filmes?filtro=em-breve">Em Breve</a>
+                        <a class="botao-padrao <?php echo "$classeEmbreve"; ?>" href="/filmes?filtro=embreve">Em Breve</a>
                     </div>
                 </div>
                 <div class="row">
@@ -61,19 +69,14 @@
                 </div>
             </div>
         </div>
-        <?php if(!empty($filtro)) { ?>
-            <a class="botao-padrao botao-padrao--preto botao-limpar-filtros-mobile" href="/filmes">Limpar Filtros</a>
-        <?php } ?>
     <?php } else { ?>
         <div class="filtros container--max">
             <span class="filtros__titulo">Filtros:</span>
+            <a class="botao-padrao <?php echo "$classeTodos"; ?>" href="/filmes">Todos</a>
             <a class="botao-padrao <?php echo "$classeCartaz"; ?>" href="/filmes?filtro=cartaz">Em Cartaz</a>
-            <a class="botao-padrao <?php echo "$classeEmbreve"; ?>" href="/filmes?filtro=em-breve">Em Breve</a>
+            <a class="botao-padrao <?php echo "$classeEmbreve"; ?>" href="/filmes?filtro=embreve">Em Breve</a>
             <a class="botao-padrao <?php echo "$classeCatalogo"; ?>" href="/filmes?filtro=catalogo">Catálogo</a>
             <a class="botao-padrao <?php echo "$classeEmCasa"; ?>" href="/filmes?filtro=emcasa">Assistir em Casa</a>
-            <?php if(!empty($filtro)) { ?>
-                <a class="botao-padrao botao-padrao--preto" href="/filmes">Limpar Filtros</a>
-            <?php } ?>
         </div>
     <?php } ?>
 <?php } ?>
