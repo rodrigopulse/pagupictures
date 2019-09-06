67621846af8dd67b94088412ad4689efe9af687c
rodrigopulse <contato@rodrigogodoy.net>
2019-09-06 11:34:53 -0300

    exibindo ordem dos filmes pelos filtros

--- archive-filmes.php
+++ archive-filmes.php
@@ -13,9 +13,29 @@ if (!empty($filtro)) {
     ) );
 } else {
     $filtro = '';
-    $the_query = new WP_Query( array(
+    $the_query1 = new WP_Query( array(
         'post_type'     => 'filmes',
         'post_status'   => 'publish',
+        'meta_key'      => 'selo',
+        'meta_value'    => 'cartaz'
+    ) );
+    $the_query2 = new WP_Query( array(
+        'post_type'     => 'filmes',
+        'post_status'   => 'publish',
+        'meta_key'      => 'selo',
+        'meta_value'    => 'embreve'
+    ) );
+    $the_query3 = new WP_Query( array(
+        'post_type'     => 'filmes',
+        'post_status'   => 'publish',
+        'meta_key'      => 'selo',
+        'meta_value'    => 'emcasa'
+    ) );
+    $the_query4 = new WP_Query( array(
+        'post_type'     => 'filmes',
+        'post_status'   => 'publish',
+        'meta_key'      => 'selo',
+        'meta_value'    => 'catalogo'
     ) );
 }
 
@@ -26,16 +46,63 @@ filtros($filtro);
     echo '<div class="container">';
 
         echo '<div class="row">';
-            while ( $the_query->have_posts() ) {
-                $the_query->the_post();
-                echo '<div class="col-sm-12 col-md-3 container-filmes">';
-                    $thumbID    = get_field( 'cartaz', $post->ID );
-                    $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
-                    $titulo     = get_the_title();
-                    $link       = get_the_permalink();;
-                    $selo       = get_field('selo', $post->ID);
-                    cardFilmes($thumb, $titulo, $link, $selo);
-                echo '</div>';
+            if (!empty($filtro)) {
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    echo '<div class="col-sm-12 col-md-3 container-filmes">';
+                        $thumbID    = get_field( 'cartaz', $post->ID );
+                        $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
+                        $titulo     = get_the_title();
+                        $link       = get_the_permalink();;
+                        $selo       = get_field('selo', $post->ID);
+                        cardFilmes($thumb, $titulo, $link, $selo);
+                    echo '</div>';
+                }
+            } else {
+                while ( $the_query1->have_posts() ) {
+                    $the_query1->the_post();
+                    echo '<div class="col-sm-12 col-md-3 container-filmes">';
+                        $thumbID    = get_field( 'cartaz', $post->ID );
+                        $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
+                        $titulo     = get_the_title();
+                        $link       = get_the_permalink();;
+                        $selo       = get_field('selo', $post->ID);
+                        cardFilmes($thumb, $titulo, $link, $selo);
+                    echo '</div>';
+                }
+                while ( $the_query2->have_posts() ) {
+                    $the_query2->the_post();
+                    echo '<div class="col-sm-12 col-md-3 container-filmes">';
+                        $thumbID    = get_field( 'cartaz', $post->ID );
+                        $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
+                        $titulo     = get_the_title();
+                        $link       = get_the_permalink();;
+                        $selo       = get_field('selo', $post->ID);
+                        cardFilmes($thumb, $titulo, $link, $selo);
+                    echo '</div>';
+                }
+                while ( $the_query3->have_posts() ) {
+                    $the_query3->the_post();
+                    echo '<div class="col-sm-12 col-md-3 container-filmes">';
+                        $thumbID    = get_field( 'cartaz', $post->ID );
+                        $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
+                        $titulo     = get_the_title();
+                        $link       = get_the_permalink();;
+                        $selo       = get_field('selo', $post->ID);
+                        cardFilmes($thumb, $titulo, $link, $selo);
+                    echo '</div>';
+                }
+                while ( $the_query4->have_posts() ) {
+                    $the_query4->the_post();
+                    echo '<div class="col-sm-12 col-md-3 container-filmes">';
+                        $thumbID    = get_field( 'cartaz', $post->ID );
+                        $thumb      = wp_get_attachment_image_url( $thumbID , 'cartaz' );
+                        $titulo     = get_the_title();
+                        $link       = get_the_permalink();;
+                        $selo       = get_field('selo', $post->ID);
+                        cardFilmes($thumb, $titulo, $link, $selo);
+                    echo '</div>';
+                }
             }
         echo '</div>' /*.row */;
     echo '</div>'/*.container */;
