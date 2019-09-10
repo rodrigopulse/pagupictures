5312c1aa58cdd90123853fa4fe3d6470a5379e29
rodrigopulse <contato@rodrigogodoy.net>
2019-09-10 17:33:35 -0300

    logo vertical

--- assets/js/componentes/menu.js
+++ assets/js/componentes/menu.js
@@ -1,29 +1,5 @@
 ( function($) {
     $(document).ready(function(){
-        var lastScrollTop = 0;
-        function menuMovimentacao() {
-            var y = $(window).scrollTop();
-            if (y > lastScrollTop){
-                if ($(".header").hasClass("header-logado")) {
-                    $(".header").css({'top': '-30px'});
-                } else{
-                    $(".header").css({'top': '-60px'});
-                }
-            } else {
-                if ($(".header").hasClass("header-logado")) {
-                    $(".header").css({'top': '30px'});
-                } else {
-                    $(".header").css({'top': '0'});
-                }
-            }
-            lastScrollTop = y;
-        }
-        menuMovimentacao();
-
-		// Window: Scroll
-		$(window).scroll(function () {
-			menuMovimentacao();
-		});
         $(".js-controla-menu").on('click', function () {
             $('.menu').toggleClass('menu-mobile-ativo');
         });
